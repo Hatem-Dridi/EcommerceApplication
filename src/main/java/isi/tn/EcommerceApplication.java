6a7885be8f1dfdd1a2897769b1855bf470963692
hatem <dridihatem1396@gmail.com>
2024-02-21 18:50:37 +0100

    Initial commit

---
+++ src/main/java/isi/tn/EcommerceApplication.java
@@ -0,0 +1,15 @@
+package isi.tn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages="isi.tn")
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
